5795010b7473c469139b7c565e5ac52986c5805b
Pheng Male <male.pheng@kmaker.io>
2024-07-19 08:45:44 +0700

    rename constant file from Constant -> constant

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,14 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: UpdateTodoTaskView
     },
-    {
-      path: '/random',
-      name: 'random-vie',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: ()=> import('@/views/RandomCopyPast.vue')
-    },
+    // {
+    //   path: '/random',
+    //   name: 'random-vie',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: ()=> import('@/views/RandomCopyPast.vue')
+    // },
         // {
       // path: '/create',
       // name: 'create-list',
